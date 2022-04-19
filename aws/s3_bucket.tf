5944cd5dc1c4bca44b55312e795038e862aaecbb
Fai Cheng <zerofai@gmail.com>
2022-04-19 15:00:18 +0800

    update s3 config

--- aws/s3_bucket.tf
+++ aws/s3_bucket.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tenable_cs_demo_s3_bucket" {
   bucket = "tenablecsdemos3bucket"
-#  acl    = "private"
-  tags   = var.default_tags
+  #  acl    = "private"
+  tags = var.default_tags
 }
